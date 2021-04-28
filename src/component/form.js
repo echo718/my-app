43f29abcf4e9156d7f9486dcc853b63719e38dea
echo718 <tong.sun@whitireianz.ac.nz>
2021-04-28 23:25:54 +1200

    28/04/2021

---
+++ src/component/form.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import '../App.css';
+
+class Form extends React.Component {   
+    constructor(props)
+    {
+        super(props);
+        this.state={value:'',
+                    age:'',
+                    gender:'Female',
+                    phone:'',
+                    message:''};
+
+        this.Submit=this.Submit.bind(this);
+        this.handleChange=this.handleChange.bind(this);
+    };
+    Submit(event){
+        if(!this.state.value){
+            alert("Please input your name");
+        }else if(!(/^[0-9]+$/.test( this.state.age ))||this.state.age<0 || this.state.age > 100 ){
+            alert("Please input right age(1-100)");
+        }else{
+            alert("Name:"+this.state.value+"\rAge:"+this.state.age+"\rGender:"+this.state.gender+"\rPhone:"+this.state.phone+"\rMessage" +this.state.message);
+        }
+        event.preventDefault();
+    };
+    handleChange(event){
+        this.setState({[event.target.name]:event.target.value });
+    }
+  render(){return (
+        
+            <form onSubmit={this.Submit} >
+               <label>
+                   <p>Name:</p>
+                   <input type="text"  name="value" value={this.state.value} onChange={this.handleChange} placeholder="Your Name"/>
+                </label>
+               <label>
+                   <p>Age:</p>
+                   <input type="text" name="age" value={this.state.age} onChange={this.handleChange} placeholder="Your Age"/>
+                </label>
+                <label>
+                   <p>Gender:</p>
+                   <select name="gender" value={this.state.gender} onChange={this.handleChange}>          
+                     <option value="Female">Female</option>
+                     <option value="Male">Male</option>
+                  </select>
+                </label>
+               <label>
+                   <p>Phone:</p>
+                   <input type="text" name="phone" value={this.state.phone} onChange={this.handleChange} placeholder="Your Phone Number"/>
+                </label>
+               <label>
+                   <p>Message:</p>
+                   <input type="text" name="message" value={this.state.message} onChange={this.handleChange} placeholder="What you want us konw"/>
+                </label>
+                <br/>
+                <input type="submit" value="Submit" id="btn"/>
+                
+               
+            </form>
+       
+      );
+  }
+ 
+}
+
+
+
+export default Form;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Form from './component/form';
+import Nav from './component/nav';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Nav />,
+    <Form />
+
   </React.StrictMode>,
   document.getElementById('root')
 );
